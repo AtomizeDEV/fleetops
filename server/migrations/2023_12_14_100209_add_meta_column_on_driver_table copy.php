e5d6f05f1112771b7fea1a8a6370853531f9dd14
Winston <winston@atomize.dev>
2024-02-21 22:52:05 +0100

    Update 2023_12_14_100209_add_meta_column_on_driver_table copy.php

--- server/migrations/2023_12_14_100209_add_meta_column_on_driver_table copy.php
+++ server/migrations/2023_12_14_100209_add_meta_column_on_driver_table copy.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddMetaColumnOnDriverTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('drivers', function (Blueprint $table) {
-            // Check if the column does not exist before adding it
-            if (!Schema::hasColumn('drivers', 'meta')) {
-                $table->json('meta')->nullable()->after('slug');
-            }
+            $table->json('meta')->nullable()->after('slug');
         });
     }
 
@@ -28,10 +25,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('drivers', function (Blueprint $table) {
-            if (Schema::hasColumn('drivers', 'meta')) {
-                $table->dropColumn('meta');
-            }
-        });
+        //
     }
-};
+}
